11d070336ee0d5e9285a3a03e3d276fa8e0b6c15
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2023-09-12 00:14:23 -0300

    removido container 'artisan' // alterado perm storage // criada migrations iniciais // instalado passport e pest php

--- src/database/migrations/2023_09_12_022250_create_musics.php
+++ src/database/migrations/2023_09_12_022250_create_musics.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('musics', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->uuid();
+            $table->foreignId('album_id')->constrained(
+                table: 'albums', indexName: 'musics_album_id'
+            );
+            $table->foreignId('gender_id')->constrained(
+                table: 'genres', indexName: 'musics_genres_id'
+            );
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('musics');
     }
 };
--- src/tests/Unit/ExampleTest.php
+++ src/tests/Unit/ExampleTest.php
@@ -2,15 +2,4 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+test('Pest PHP Unit', fn () => expect(true)->toBeTrue());
