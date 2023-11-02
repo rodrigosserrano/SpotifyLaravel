59b6f1b77593c9452dcc2a479c3d84d5e6706e70
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2023-11-01 23:51:44 -0300

    Add plugin past dependence

---
+++ src/tests/Feature/GoogleSocialiteServiceTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Feature;
+
+use Laravel\Socialite\Facades\Socialite;
+
+test('Login with Google Socialite', function () {
+    $abstractUser = \Mockery::mock('Laravel\Socialite\Two\User');
+});
