41134f21062c2298edbb74a0efeae65cffffad94
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2023-11-01 23:54:38 -0300

    Added tests google socialite

--- src/tests/Feature/GoogleSocialiteServiceTest.php
+++ src/tests/Feature/GoogleSocialiteServiceTest.php
@@ -6,4 +6,17 @@ use Laravel\Socialite\Facades\Socialite;
 
 test('Login with Google Socialite', function () {
     $abstractUser = \Mockery::mock('Laravel\Socialite\Two\User');
+    $abstractUser
+        ->shouldReceive('getId')
+        ->andReturn(fake()->numerify('#####################'))
+        ->shouldReceive('getName')
+        ->andReturn(fake()->name())
+        ->shouldReceive('getEmail')
+        ->andReturn(fake()->unique()->safeEmail())
+        ->shouldReceive('getAvatar')
+        ->andReturn(fake()->imageUrl(category: 'person'));
+
+    Socialite::shouldReceive('driver->user')->andReturn($abstractUser);
+
+    $this->get('/oauth/google/callback')->assertRedirect('/');
 });
